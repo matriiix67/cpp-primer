559f08dea7286161e5666086e4a13acf9cc57766
matriiix67 <matriiix67@gmail.com>
2016-06-22 14:11:41 +0800

    add exercise 4.33-4.38

---
+++ ch04/exercise4.34.cpp
@@ -0,0 +1,3 @@
+a) fval转换成bool类型，如果flag=0，则为false，否则为true;
+b) ival先转换成float跟fval相加，再转换成double;
+c) cval 转换成int与ival相乘，再转换成double跟dval相加
---
+++ ch04/exercise4.35.cpp
@@ -0,0 +1,4 @@
+a) 'a'转换成int跟3相加，再转换成char类型；
+b) ival转换成double跟1.0相乘，ui转换成double跟ival*1.0相减，在换成float
+c) ui转换成float跟fval相乘，在转成double
+d) ival换成float跟fval相加，再换成double跟dval相加，在换成char
